245d27062ca603f682a482edd3fcbe2ea58ec393
George Muscat <george@muscat.sh>
2025-12-11 00:14:26 +1100

    unfinished

--- src/bin.rs
+++ src/bin.rs
@@ -1,5 +1,10 @@
+use std::{fs::File, path::PathBuf};
+
 use anyhow::Result;
 use loggalib::comms::recv::Server;
+use tokio::sync::mpsc;
+
+use crate::module::{FileSink, FileSource};
 
 mod agent;
 mod module;
@@ -9,7 +14,32 @@ async fn main() -> Result<()> {
     rustls::crypto::ring::default_provider()
         .install_default()
         .expect("Failed to install rustls crypto provider");
-    println!("test");
 
+    // TODO: do config parsing here once we have it implemented.
+
+    // For now, just do simple file input and output
+    // TODO: handle errors better
+
+    // Need to construct the consumers first, so that we can create the required
+    // channels to pass to the producers/senders
+    //
+    // TODO: When configs eventually become a thing, we would essentially construct a graph of flows,
+    // and create mpsc channels from "right to left" or "sink to source"
+    let (mut send, mut recv) = mpsc::channel::<&[u8]>(100); // TODO: create a sensible default and a method to customise based on Bytes or something
+    let output_path = PathBuf::from("./test-out.log");
+    let delimiter = "\n".as_bytes();
+    let output = FileSink::new("Sink1".to_string(), output_path.clone(), delimiter, recv).await?;
+
+    let input_path = PathBuf::from("./test-in.log");
+    dbg!(&input_path);
+    let input =
+        FileSource::new_with_channels("Source1".to_string(), input_path, delimiter, vec![send])
+            .await?;
+
+    // Create threads, one per module for now.
+    // TODO: Consider how we handle one of the threads crashing/stopping.
+    // TODO: In future there might be smarter ways to do threading.
+    tokio::spawn(output.start());
+    tokio::spawn(input.start());
     Ok(())
 }
--- src/module.rs
+++ src/module.rs
@@ -1,5 +1,10 @@
-use std::{fs::File, net::Ipv4Addr, path::PathBuf};
-
+use anyhow::Result;
+use std::{net::Ipv4Addr, path::PathBuf};
+use tokio::{
+    fs::File,
+    io::{AsyncBufRead, AsyncReadExt, BufReader},
+    sync::mpsc::{Receiver, Sender},
+};
 trait Module<I, O> {
     fn read(&self, inp: I) -> O;
 }
@@ -7,19 +12,90 @@ trait Module<I, O> {
 // Make this module implement whatever trait is "Input" as it provides a stream to be read from
 trait Source {}
 
-// Maket his module implement whatever trais is "Output" as it provides a stream to
+// When implementing processing, might be cool to use `rayon` for things like dedupe or
+
+// Make his module implement whatever trais is "Output" as it provides a stream to
 trait Sink {}
 
-pub struct FileSource {
+pub struct FileSource<'a, T> {
     // Going to start with an implementation that should be simple and
     // Need to decide how to handle the case where the file moves, both when we are in the middle or reading or not currently reading.
+    // https://docs.rs/inotify/latest/inotify/ can maybe use this to have really efficient reading of files
+
+    // This should only be used for logging or showing where this struct was constructed from in a config module. May also want to have another method for this.
+    // Careful using this, as path will not change if the file at the path at the time of initialisation is moved.
+    // This means it is possible for `FileSource.path` and `FileSource.file` to be referring to different files.
+    name: String,
     path: PathBuf,
-    fp: File,
+    file: File,
+    delimiter: &'a [u8],
+    out_chans: Vec<Sender<T>>,
 }
 
-pub struct FileSink {
+pub struct FileSink<'a, T> {
+    name: String,
     path: PathBuf,
-    fp: File,
+    file: File,
+    delimiter: &'a [u8],
+    inp_chan: Receiver<T>,
+}
+
+impl<'a, T> FileSource<'a, T> {
+    pub async fn new(name: String, path: PathBuf, delimiter: &'a [u8]) -> Result<Self> {
+        Self::new_with_channels(name, path, delimiter, vec![]).await
+    }
+
+    pub async fn new_with_channels(
+        name: String,
+        path: PathBuf,
+        delimiter: &'a [u8],
+        channels: impl IntoIterator<Item = tokio::sync::mpsc::Sender<T>>,
+    ) -> Result<Self> {
+        // Open this here, because we want to stop
+        let file = File::open(&path).await?;
+        Ok(Self {
+            name,
+            path,
+            file,
+            delimiter,
+            out_chans: channels.into_iter().collect(),
+        })
+    }
+
+    pub fn register_channel(&mut self, channel: Sender<T>) -> Result<()> {
+        self.out_chans.push(channel);
+        Ok(())
+    }
+
+    pub async fn start(self) -> Result<()> {
+        let mut reader = BufReader::new(self.file.try_clone().await?);
+        loop {
+            // Keep reading from the file until EOF.
+            // TODO: impl
+        }
+    }
+}
+
+impl<'a, T> FileSink<'a, T> {
+    pub async fn new(
+        name: String,
+        path: PathBuf,
+        delimiter: &'a [u8],
+        recv: Receiver<T>,
+    ) -> Result<Self> {
+        let file = File::open(&path).await?;
+        Ok(Self {
+            name,
+            path,
+            file,
+            delimiter,
+            inp_chan: recv,
+        })
+    }
+
+    pub async fn start(self) -> Result<()> {
+        Ok(())
+    }
 }
 
 pub struct QUICSource {
